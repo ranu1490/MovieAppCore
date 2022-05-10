869810b8ad4c8a591c93cf892490a30893778b8b
ranu1490 <ranusinghrajput75@gmail.com>
2022-05-10 12:21:17 +0530

    Movie App

---
+++ MovieAppCore.UI/Controllers/MoviesController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MovieAppCore.Entity.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MovieAppCore.UI.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private IConfiguration _configuration;
+        public MoviesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Index()        
+        {
+            IEnumerable<Movie> movieresult = null;
+            using (HttpClient client = new HttpClient())
+            {
+                string endPoint = _configuration["WebApiBaseUrl"] + "Movies/GetMovies";
+                using (var response =await client.GetAsync(endPoint))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        movieresult = JsonConvert.DeserializeObject<IEnumerable<Movie>>(result);
+                    }
+                }
+            }
+            return View(movieresult);
+        }
+        public IActionResult MovieEntry()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> MovieEntry(Movie movie)
+        {
+            ViewBag.status = "";
+            if (Request.Form.Files.Count > 0)
+            {
+                MemoryStream ms = new MemoryStream();
+                Request.Form.Files[0].CopyTo(ms);
+                movie.ImgPoster = ms.ToArray();
+            }
+            using (HttpClient client=new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(movie), Encoding.UTF8, "application/json");
+                string endPoint = _configuration["WebApiBaseUrl"]+ "Movies/AddMovie";
+                using (var response =await client.PostAsync(endPoint, content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "Movie details saved successfully!";
+                    }
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong entries!";
+                    }
+                }
+            }
+            return View();
+        }
+
+    }
+}
